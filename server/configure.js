f4b70e20c9e3b310c95794b531928badccbca872
HumanTorpedo <humantorpedo55@gmail.com>
2016-01-01 22:48:59 -0600

    adding MVC

--- server/configure.js
+++ server/configure.js
@@ -7,18 +7,26 @@ var path = require('path'),
     morgan = require('morgan'),
     methodOverride = require('method-override'),
     errorHandler = require('errorhandler');
-
+   
 module.exports = function(app) {
     app.use(morgan('dev'));
-    app.use(bodyParser.urlencoded({'extended':true}));
+    app.use(bodyParser.urlencoded({extended: true}, {uploadDir:path.join(__dirname, 'public/upload/temp')}));
     app.use(bodyParser.json());
     app.use(methodOverride());
     app.use(cookieParser('some-secret-value-here'));
-    //routes(app);//moving the routes to routes folder.
+    routes(app);
     app.use('/public/', express.static(path.join(__dirname,'../public')));
 
     if ('development' === app.get('env')) {
         app.use(errorHandler());
     }
+    
+    app.engine('handlebars', exphbs.create({
+        defaultLayout: 'main',
+        layoutsDir: app.get('views') + '/layouts',
+        partialsDir: [app.get('views') + '/partials']
+        }).engine);
+    app.set('view engine', 'handlebars');
+    
     return app;
 };
